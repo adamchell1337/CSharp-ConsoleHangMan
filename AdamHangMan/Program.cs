f354d74d93131580919f443b5c4b1e4e3b7d8f79
Adam Chell <57140341+adamchell1337@users.noreply.github.com>
2020-03-01 20:47:03 +0000

    Add files via upload

---
+++ AdamHangMan/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+namespace AdamHangMan
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Adam's Hangman Game!");
+            string[] wordList = new string[10];
+            wordList[0] = "dinosaur";
+            wordList[1] = "volcano";
+            wordList[2] = "energy";
+            wordList[3] = "wilderness";
+            wordList[4] = "snowflake";
+            wordList[5] = "development";
+            wordList[6] = "placebo";
+            wordList[7] = "toothpaste";
+            wordList[8] = "waffle";
+            wordList[9] = "distribution";
+            Random rand = new Random();
+            var randomword = rand.Next(0, 9);
+            string mysteryword = wordList[randomword];
+            char[] guess = new char[mysteryword.Length];
+            Console.WriteLine("Please enter your guess: ");
+
+            for (int i = 0; i < mysteryword.Length; i++)
+                guess[i] =  '*';
+
+            while(true)
+            {
+                try
+                {
+                    char playerguess = char.Parse(Console.ReadLine());
+                    for (int j = 0; j < mysteryword.Length; j++)
+                    {
+                        if (playerguess == mysteryword[j])
+                            guess[j] = playerguess;
+                    }
+                    Console.WriteLine(guess);
+                }
+                catch
+                {
+                    Console.WriteLine("Please only guess one character at a time");
+                }
+                StringBuilder guesses = new StringBuilder();
+                foreach (char letter in guess)
+                {
+                    guesses.Append(letter);
+                    string abc = guesses.ToString();
+                    if (abc == mysteryword)
+                    {
+                        Console.WriteLine("Well Done! You have guessed the word correctly!");
+                        
+                        
+                    }
+                }
+               
+                
+            }
+        }
+    }
+}
